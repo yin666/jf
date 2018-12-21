965f5afc16c3fd3966b02b32921ecdfbf47b5f22
Bruce <lib8273@gmail.com>
2018-12-21 14:49:12 +0800

    npm install -D sass-loader node-sass create home

--- jf/jf/src/router/index.js
+++ jf/jf/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//  import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+export const index = {
+  path: '/',
+  name: 'index',
+  component: () => import('@/pages/index.vue'),
+  children:[
+		{
+			path: '',
+			title: 'home',
+			name: 'home',
+			component: () => import('@/pages/index/home.vue')
+    }
+    // {
+		// 	path: 'slog',
+		// 	title: 'slog',
+		// 	name: 'slog',
+		// 	component: () => import('@/pages/index/home.vue')
+		// }
+	]
+}
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    index
   ]
 })
